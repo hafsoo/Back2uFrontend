cc4ddff9683c77cca273758d7a3c5c9aa9aa72ba
hafsoo <hafsariazturk9@gmail.com>
2026-05-03 20:48:57 +0500

    update demo video

--- src/components/Route/DemoSection/DemoSection.jsx
+++ src/components/Route/DemoSection/DemoSection.jsx
@@ -48,7 +48,7 @@ const DemoSection = () => {
           {/* Video Preview */}
           <video
             ref={previewRef}
-            src="/images/demo.mp4"
+            src="/images/video.mp4"
             muted
             loop
             playsInline
